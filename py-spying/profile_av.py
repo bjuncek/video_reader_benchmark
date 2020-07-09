3dab18b0b872aeba39fcf2955b8111a4d151178c
Bruno Korbar <bjuncek@Calvin.local>
2020-07-09 16:44:05 -0500

    adding first profiling results

---
+++ py-spying/profile_av.py
@@ -0,0 +1,15 @@
+import os
+import av
+
+print("inports over")
+videos = [os.path.join("../videos/", x) for x in os.listdir("../videos") if x not in ["README", ".ipynb_checkpoints"]]
+
+print("Now starting video decoding for profile")
+
+for i in range(100):
+    for path in videos:
+        images_av = []
+        container = av.open(path)
+        container.streams.video[0].thread_type = 'AUTO'
+        for frame in container.decode(video=0):
+            images_av.append(frame.to_rgb().to_ndarray())
---
+++ py-spying/profile_cv2.py
@@ -0,0 +1,21 @@
+import os
+import cv2
+
+print("inports over")
+videos = [os.path.join("../videos/", x) for x in os.listdir("../videos") if x not in ["README", ".ipynb_checkpoints"]]
+
+print("Now starting video decoding for profile")
+
+for i in range(100):
+    for path in videos:
+        images_cv2 = []
+        cv2.setNumThreads(1)
+        cap = cv2.VideoCapture(path)
+        while(cap.isOpened()):
+            ret, frame = cap.read()
+            if ret is True:
+                rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                images_cv2.append(frame)
+            else:
+                break
+        cap.release()
